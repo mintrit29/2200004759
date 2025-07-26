5f7fb8db5c7ebcfe4b603303052764482cab371a
0abit <jumpalex1911@gmail.com>
2025-07-26 09:05:28 +0700

    update statistics

---
+++ NhomDangKhoa/Controllers/StatisticsController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NhomDangKhoa.Data;
+using NhomDangKhoa.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NhomDangKhoa.Controllers
+{
+    public class StatisticsController : Controller
+    {
+       
+        private readonly _22bitv02EmployeeContext _context;
+
+        public StatisticsController(_22bitv02EmployeeContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var statistics = await _context.Departments
+                .Include(d => d.Employees) 
+                .Select(d => new DepartmentStatisticsViewModel
+                {
+                    DepartmentId = d.DepartmentId,
+                    DepartmentName = d.DepartmentName,
+                    NumberOfEmployees = d.Employees.Count(),
+                    // Tính tổng lương, nếu không có nhân viên nào thì tổng lương là 0
+                    TotalSalary = d.Employees.Sum(e => e.Salary ?? 0),
+                    // Đếm số nhân viên nữ (giả định Gender: 0 là Nữ, 1 là Nam)
+                    // Dựa trên dữ liệu mẫu của bạn, Gender là kiểu bool? (nullable boolean)
+                    TotalOfFemale = d.Employees.Count(e => e.Gender == false)
+                })
+                .ToListAsync();
+
+            // Gửi danh sách thống kê đến View
+            return View(statistics);
+        }
+    }
+}
---
+++ NhomDangKhoa/Models/DepartmentStatisticsViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace NhomDangKhoa.Models
+{
+
+    public class DepartmentStatisticsViewModel
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; } = "";
+        public int NumberOfEmployees { get; set; }
+        public decimal TotalSalary { get; set; }
+        public int TotalOfFemale { get; set; }
+    }
+}
