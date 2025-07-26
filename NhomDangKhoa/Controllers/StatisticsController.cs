afc11ebc078688fbb7d70424e8ff91ed15412e09
0abit <jumpalex1911@gmail.com>
2025-07-26 09:06:49 +0700

    update statistics again

--- NhomDangKhoa/Controllers/StatisticsController.cs
+++ NhomDangKhoa/Controllers/StatisticsController.cs
@@ -27,15 +27,14 @@ namespace NhomDangKhoa.Controllers
                     DepartmentId = d.DepartmentId,
                     DepartmentName = d.DepartmentName,
                     NumberOfEmployees = d.Employees.Count(),
-                    // Tính tổng lương, nếu không có nhân viên nào thì tổng lương là 0
+                   
                     TotalSalary = d.Employees.Sum(e => e.Salary ?? 0),
-                    // Đếm số nhân viên nữ (giả định Gender: 0 là Nữ, 1 là Nam)
-                    // Dựa trên dữ liệu mẫu của bạn, Gender là kiểu bool? (nullable boolean)
+                   
                     TotalOfFemale = d.Employees.Count(e => e.Gender == false)
                 })
                 .ToListAsync();
 
-            // Gửi danh sách thống kê đến View
+
             return View(statistics);
         }
     }
